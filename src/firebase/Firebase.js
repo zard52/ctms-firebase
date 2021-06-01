9e9e356a01e39aa122053ffc05fe3f7727995a66
unknown <addtim52@gmail.com>
2021-06-01 17:49:00 +0800

    merge

--- src/firebase/Firebase.js
+++ src/firebase/Firebase.js
@@ -10,7 +10,8 @@ const app = firebase.initializeApp({
   databaseURL: process.env.REACT_APP_DATABASE_URL,
   projectId: process.env.REACT_APP_PROJECT_ID,
   messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_APP_ID
+  appId: process.env.REACT_APP_APP_ID,
+  measurementId: process.env.REACT_APP_MEASUREMENT_ID  
 })
 
 class Firebase {
